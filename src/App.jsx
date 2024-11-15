c94d035c7f2c3e130fa74a6d2602c4f3d73476ef
Kushal Patil <kushal2423@gmail.com>
2024-11-15 22:52:52 +0530

    Added About us page and added icons on  Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
-// import { Button } from "./components/ui/button"
-import Navbar from "./pages/Navbar"
+// Import necessary components and React Router modules
+import Navbar from "./pages/Navbar";
 import Home from "./pages/Home";
+import About from "./pages/About"; // Import About component only once
 
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
-
   return (
-    <>
-    
-      <Router>
-        <div >
-          <Navbar />
-        </div>
-        <Home/>
-      </Router>
-    </>
+    <Router>
+      <div>
+        <Navbar /> {/* Navbar with links */}
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/aboutus" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
-export default App
+export default App;
---
+++ src/pages/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function About() {
+  return (
+    <> 
+      <header style={{ textAlign: "center", fontSize: "2em", margin: "1em 0" }}>About Us</header>
+      <div style={{ padding: "0 1em", lineHeight: "1.6" }}>
+        Deepfakes are going to be the first real punch from AI to humanity. The cybersecurity industry has a very short time to get ahead of deepfakes before they undermine the public's trust in reality.
+        We first recognized the danger while our parent company, Zemana, researched methods to develop an AI-based antivirus engine. Later, in mid-2018, we started our research on deepfake detection and generation as the Deepware AI team.
+      </div>
+    </>
+  );
+}
+
+export default About;
